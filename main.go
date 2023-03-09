34e951ed059cdbeaedbd8cad6629000df0c00946
Y3llowm4n <mikegeelen2002@gmail.com>
2023-03-09 13:14:58 +0100

    genPassword mod.
    code aanpassen meer efficient

--- main.go
+++ main.go
@@ -1,21 +1,43 @@
 // input wachtwoord geven
 // functie wachtwoord genereren
 // functie hashing
+
+// username toevoegen
+// username duplicate error
+// database connecten met file
+// error schrijven naar logfile
 package main
 
 import (
+	"flag"
 	"fmt"
 	"math/rand"
 	"time"
 )
 
 // global variable which includes all the letters, numbers and tokens.
-var letters = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()")
+var (
+	letters        = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()")
+	lengthPassword int
+)
+
+func init() {
+	flag.IntVar(&lengthPassword, "p", 12, "Give a value for the length off your password")
+	flag.Parse()
+}
 
 func main() {
+	username := getUser()
+	fmt.Println("Name:", username)
 	genPassword()
 }
 
+func getUser() string {
+	var username string
+	fmt.Println("Please give a username")
+	fmt.Scanln(&username)
+	return username
+}
 func genPassword() {
 
 	rand.Seed(time.Now().UnixNano())
@@ -26,13 +48,11 @@ func genPassword() {
 
 	str := str1 + str2 + str3
 
-	n := 13
-
-	p := make([]rune, n)
+	p := make([]rune, lengthPassword)
 
 	for i := range p {
 		p[i] = letters[rand.Intn(len(str))]
 	}
 
-	fmt.Println(string(p))
+	fmt.Println("Password:", string(p))
 }
